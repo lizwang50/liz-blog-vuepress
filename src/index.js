6e53f71e9a2466a69b6cf5b03b4b1ab781251886
Liz Wang <erossatan33@gmail.com>
2020-07-15 15:27:23 +0800

    delete bg_1.jpg

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ module.exports = (themeConfig, ctx) => {
         : 250
   });
 
-  themeConfig.heroImage = themeConfig.heroImage || "./images/bg_1.jpg";
+  themeConfig.heroImage = themeConfig.heroImage || "./images/bg_2.jpg";
 
   const defaultBlogPluginOptions = {
     directories: [
